f04001aa822a06beed9391802a35046bb08902a1
Ryan McCall <ryan.mccall@codeup.com>
2022-06-13 15:21:38 -0500

    Update outlier function

--- wrangle_mall.py
+++ wrangle_mall.py
@@ -55,15 +55,35 @@ def overview(df):
     print('--- Column Descriptions')
     print(df.describe(include='all'))
 
-def outlier_function(df, cols, k):
-    for col in df[cols]:
+def handle_outliers(df, cols, k):
+    # Create placeholder dictionary for each columns bounds
+    bounds_dict = {}
+
+    # get a list of all columns that are not object type
+    non_object_cols = df.dtypes[df.dtypes != 'object'].index
+
+
+    for col in non_object_cols:
+        # get necessary iqr values
         q1 = df[col].quantile(0.25)
         q3 = df[col].quantile(0.75)
         iqr = q3 - q1
         upper_bound =  q3 + k * iqr
-        lower_bound =  q1 - k * iqr     
-        df = df[(df[col] < upper_bound) & (df[col] > lower_bound)]
-    return df
+        lower_bound =  q1 - k * iqr
+
+        #store values in a dictionary referencable by the column name
+        #and specific bound
+        bounds_dict[col] = {}
+        bounds_dict[col]['upper_bound'] = upper_bound
+        bounds_dict[col]['lower_bound'] = lower_bound
+
+    for col in non_object_cols:
+        #retrieve bounds
+        col_upper_bound = bounds_dict[col]['upper_bound']
+        col_lower_bound = bounds_dict[col]['lower_bound']
+
+        #remove rows with an outlier in that column
+        df = df[(df[col] < col_upper_bound) & (df[col] > col_lower_bound)]
 
 def handle_missing_values(df, prop_required_column, prop_required_row):
     n_required_column = round(df.shape[0] * prop_required_column)
@@ -93,7 +113,7 @@ def get_modeling_data(scale_data=False):
     print()
     
     print('Before removing outliers, %d rows, %d cols' % df.shape)
-    outlier_function(df, ['age', 'spending_score', 'annual_income'], 1.5)
+    handle_outliers(df, ['age', 'spending_score', 'annual_income'], 1.5)
     print('After dropping nulls, %d rows, %d cols' % df.shape)
     
     print()
